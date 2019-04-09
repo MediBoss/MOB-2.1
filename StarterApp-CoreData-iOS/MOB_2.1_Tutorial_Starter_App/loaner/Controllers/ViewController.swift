8ea38ec06d70641a776c5afb79c31f9de1842e53
Medi Assumani <mediassumani49@gmail.com>
2019-04-09 15:35:04 -0700

    [Add] saving to the persistent store

--- StarterApp-CoreData-iOS/MOB_2.1_Tutorial_Starter_App/loaner/Controllers/ViewController.swift
+++ StarterApp-CoreData-iOS/MOB_2.1_Tutorial_Starter_App/loaner/Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
@@ -28,8 +29,17 @@ class ViewController: UIViewController {
         collectionView.collectionViewLayout = flow
     }
 
+
     func createNewItem() -> Item {
-        return Item(itemTitle: "Untitled Item")
+        let newItem = NSEntityDescription.insertNewObject(forEntityName: "Item", into: store.persistentContainer.viewContext) as! Item
+        return newItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        // Save the new items in the Managed Object Context
+        store.saveContext()
     }
     
     func add(saved item: Item) {
