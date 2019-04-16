f16787ce4048faf88efc9e7db8985d520b37051e
Medi Assumani <mediassumani49@gmail.com>
2019-04-16 13:21:19 -0700

    [Add] models

---
+++ TripStop/TripStop/Models/Trip.swift
@@ -0,0 +1,16 @@
+//
+//  Trip.swift
+//  TripStop
+//
+//  Created by Medi Assumani on 4/16/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import Foundation
+
+struct Trip {
+    
+    var name: String?
+    var waypoint: [Waypoint]?
+    
+}
---
+++ TripStop/TripStop/Models/Waypoint.swift
@@ -0,0 +1,16 @@
+//
+//  Waypoint.swift
+//  TripStop
+//
+//  Created by Medi Assumani on 4/16/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+import CoreLocation
+import Foundation
+
+struct Waypoint{
+    
+    var name: String?
+    var coordinates: CLLocationCoordinate2D?
+}
